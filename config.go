758cd05f4856ca936a7108fd9b498ba15490c0f4
Zheng MingZhi <18219111852@163.com>
2020-02-21 16:56:00 +0800

    update config

--- config.go
+++ config.go
@@ -5,9 +5,35 @@ import (
 	"github.com/fsnotify/fsnotify"
     "github.com/spf13/viper"
 )
+
+//.Cache(config)
+type Server struct {
+	Host string `json:"host" mapstructure:"host"`
+	Port uint32 `json:"port" mapstructure:"port"`
+}
+type TokenCfg struct {
+	Version string `json:"version" mapstructure:"version"`
+	Et      string `json:"et" mapstructure:"et"`
+	Method  string `json:"method" mapstructure:"method"`
+}
+type OneNet struct {
+	ClientId    string `json:"Client-id" mapstructure:"Client-id"`
+	KeepAlive   uint32 `json:"KeepAlive" mapstructure:"KeepAlive"`
+	PingTimeout uint32 `json:"PingTimeout" mapstructure:"PingTimeout"`
+	Username    string `json:"Username" mapstructure:"Username"`
+	AccessKey    string `json:"AccessKey" mapstructure:"AccessKey"`
+	Server      Server `json:"Server" mapstructure:"Server"`
+	Token       TokenCfg `json:"Token" mapstructure:"Token"`
+}
+type Configuration struct {
+	OneNet   OneNet `json:"OneNet" mapstructure:"OneNet"`
+	LogLevel string `json:"log-level" mapstructure:"log-level"`
+}
+
 func AddConfig(name string) (c *viper.Viper) {
 	c = viper.New()
 	c.SetConfigName(name)
+	viper.SetConfigType("json")
 	c.AddConfigPath("/etc/config")
 	c.AddConfigPath("/etc")
 	c.AddConfigPath(".")
@@ -18,4 +44,10 @@ func AddConfig(name string) (c *viper.Viper) {
 		fmt.Println("Config file changed:", e.Name)
 	})
 	return
+}
+
+func InitConfig() error {
+	cfg := AddConfig("config")
+	err := cfg.Unmarshal(&config)
+	return err
 }
